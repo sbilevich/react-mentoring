fe5b71d7b7e561cdf4b0a7ba183d2927886e971c
Svetlana_Bilevich <svetlana_bilevich@epam.com>
2022-10-16 15:05:15 +0400

    task 1

---
+++ src/components/pure-component.tsx
@@ -0,0 +1,9 @@
+import React, { PureComponent } from "react";
+
+export class Component extends PureComponent {
+    render () {
+        return (
+            <div>Hello, World!</div>
+        )
+    }
+}
