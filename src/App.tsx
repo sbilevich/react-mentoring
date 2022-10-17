f6b5249e5d6946ce9ad68513ac75030fe7291533
Svetlana_Bilevich <svetlana_bilevich@epam.com>
2022-10-17 12:04:37 +0400

    undo

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
-import React from 'react';
-import './App.css';
-import {Component} from "./components/component";
-import {PureComponent} from "./components/pure-component";
-import {FunctionalComponent} from "./components/functional-component";
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Component />
-      <PureComponent />
-      <FunctionalComponent />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
