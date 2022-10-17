d06172d7a8e224bfa8d6ff44feae336b27853f6d
Svetlana_Bilevich <svetlana_bilevich@epam.com>
2022-10-17 11:50:11 +0400

    task1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Component } from "./components/component";
+import { PureComponent } from "./components/pure-component";
+import { FunctionalComponent } from "./components/functional-component";
+import { Counter } from "./components/counter";
 
 function App() {
   return (
     <div className="App">
-
+      <Component />
+      <PureComponent />
+      <FunctionalComponent />
+      <Counter startValue={0} />
     </div>
   );
 }
---
+++ src/components/pure-component.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export class PureComponent extends React.PureComponent {
+    render () {
+        return (
+            <div>Hello, World!</div>
+        )
+    }
+}
