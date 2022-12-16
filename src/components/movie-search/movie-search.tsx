44c0bc06a381de2571d98259f9ece858088b79e7
Svetlana Bilevich <svetlana_bilevich@epam.com>
2022-12-17 00:18:28 +0400

    fixes

--- src/components/movie-search/movie-search.tsx
+++ src/components/movie-search/movie-search.tsx
@@ -1,11 +1,9 @@
 import { Search } from '../search/search';
 import styles from './movie-search.module.scss';
 
-export const MovieSearch = () => {
-  return (
-    <div className={styles.searchWrapper}>
-      <h1 className={styles.text}>Find your movie</h1>
-      <Search />
-    </div>
-  );
-};
+export const MovieSearch = () => (
+  <div className={styles.searchWrapper}>
+    <h1 className={styles.text}>Find your movie</h1>
+    <Search />
+  </div>
+);
